e2e4bc3dd30e65d16306fb05f6e39b736390d239
gets23 <geget@192.168.1.56>
2025-10-27 18:00:02 +0700

    earth wind n fire

---
+++ public/about.php
@@ -0,0 +1,9 @@
+<?php 
+
+?>
+    </main>
+
+    <footer class="bg-white border-t">
+        <div class="page-container px-4 py-6 text-center text-sm text-gray-500">
+            © <?= date('Y') ?> 
+        </div>
